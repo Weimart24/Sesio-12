51c45879da1f1a2a89511f748b26452120d137db
Weimart24 <weimart24@gmail.com>
2022-12-05 18:42:48 -0500

    corregí un error grave

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const navToggle = document.querySelector(".nav-toggle");
-const navMenu = document.querySelector(".nave-menu");
+const navMenu = document.querySelector(".nav-menu");
 
 navToggle.addEventListener("click", () => {
-    navMenu.classList.toggle("nav-menu-visible");
+    navMenu.classList.toggle("nav-menu_visible");
 
 if(navMenu.classList.contains("nav-menu_visible")){
     navToggle.setAttribute("aria-label", "Cerrar menú");
